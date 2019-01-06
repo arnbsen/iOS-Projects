8a6e13e70c683906336d3f67c274aac74df02ebf
Arnab Sen <arnb.sen@hotmail.com>
2019-01-06 11:24:24 +0530

    Refactored Code

--- Set/Set/Set/ViewController.swift
+++ Set/Set/Set/ViewController.swift
@@ -10,13 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let shape = ["▲", "●", "■"]
-    let shadingStroke = [-2.0 ,-2.0 ,3.0]
-    let shadingAlpha = [ 1.0 , 0.25, 0.0]
-    let colorValues  = [(243.0/255.0, 156.0/255.0, 18.0/255.0), (192.0/255.0, 57.0/255.0, 43/255.0), (52/255.0, 152/255.0, 219/255.0)]
+    private let shape = ["▲", "●", "■"]
+    private let shadingStroke = [-2.0 ,-2.0 ,3.0]
+    private let shadingAlpha = [ 1.0 , 0.25, 0.0]
+    private let colorValues  = [(243.0/255.0, 156.0/255.0, 18.0/255.0), (192.0/255.0, 57.0/255.0, 43/255.0), (52/255.0, 152/255.0, 219/255.0)]
     
     private lazy var game = Set()
-    private lazy var visibleCards = [Card?]()
     
     
     @IBOutlet private var cardButtons: [UIButton]!
@@ -43,16 +42,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for card in game.activePlayingCards {
-            visibleCards += [card]
-        }
-        for _ in 1...12 {
-            visibleCards += [nil]
-        }
-        var temp_array = visibleCards
-        for index in visibleCards.indices {
-            visibleCards[index] = temp_array.remove(at: temp_array.count.arc4random)
-        }
         updateViewFromModel()
     }
     
