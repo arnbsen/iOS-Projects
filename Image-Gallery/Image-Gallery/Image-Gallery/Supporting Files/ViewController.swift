38e3e9270f09600b2b93d37c6ff1a14b853474a9
Arnab Sen <arnb.sen@hotmail.com>
2019-02-03 13:24:33 +0530

    Initialised Image-Gallery (Project 5)

---
+++ Image-Gallery/Image-Gallery/Image-Gallery/Supporting Files/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image-Gallery
+//
+//  Created by Arnab Sen on 03/02/19.
+//  Copyright © 2019 Arnab Sen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
